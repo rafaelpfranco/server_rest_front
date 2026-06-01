101ed1a4e57103581b549b1afb68feba9ab9da64
Rafael Franco <rafael.gouveia@squadra.com.br>
2026-06-01 17:12:20 -0300

    chore: configuracao inicial do projeto

---
+++ cypress.config.js
@@ -0,0 +1,30 @@
+const { defineConfig } = require('cypress')
+require('dotenv').config()
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: process.env.BASE_URL || 'https://front.serverest.dev',
+    setupNodeEvents(on) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+    },
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.js',
+  },
+  env: {
+    userEmail: process.env.USER_EMAIL,
+    userPassword: process.env.USER_PASSWORD,
+    tipoTeste: process.env.TIPO_TESTE || 'regressivo',
+  },
+  video: true,
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
+  downloadsFolder: 'cypress/downloads',
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+})
